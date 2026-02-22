1a6281a149529982b6f0031397323c6454aa7070
Scott Burman <scottburs@gmail.com>
2026-02-22 15:26:08 -0600

    updated for branch walk through

--- Week 1 - Day 2 - Core Java Fundamentals — Part 1/Code-walkthrough/01-Architecture-and-Primitives/SimpleProgram.java
+++
@@ -1,8 +0,0 @@
-public class SimpleProgram {
-    public static void main(String[] args) {
-        int x = 5;
-        int y = 10;
-        int sum = x + y;
-        System.out.println("5 + 10 = " + sum);
-    }
-}
